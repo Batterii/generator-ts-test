b139ea73e7f5635077aa5827a4cf95cb3ddd503a
Steve Ripberger <sripberger@batterii.com>
2020-02-06 11:28:52 -0500

    Improve filename generation

--- generators/app/utils.js
+++ generators/app/utils.js
@@ -1,8 +1,10 @@
+const decamelize = require('decamelize');
+
 exports.validateName = function(name) {
 	return (/^[\w- ]+$/).test(name) ||
 		'Test name may contain only word characters, dashes, and spaces';
 };
 
 exports.getFilename = function(name) {
-	return name.toLowerCase().replace(/[\W_]+/g, '-');
+	return decamelize(name).replace(/[\W_]+/g, '-');
 };

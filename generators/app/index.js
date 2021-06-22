232a904f8c450d5860b838fa8fd718c55534bb31
Steve Ripberger <steve.ripberger@gmail.com>
2021-06-22 12:46:36 -0400

    Upgrade to latest eslint and linter rules and fix errors

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,28 +2,28 @@ const {
 	getDefaultFilename,
 	validateFilename,
 	validateName,
-} = require('./utils');
-const { Generator } = require('@batterii/yeoman-helpers');
+} = require("./utils");
+const {Generator} = require("@batterii/yeoman-helpers");
 
 class TestGenerator extends Generator {
 	constructor(args, opts) {
 		super(args, opts);
 
 		this.optionPrompt({
-			type: 'input',
-			name: 'name',
-			alias: 'n',
-			description: 'Name for the test block',
-			message: 'Enter a name for the test block.',
+			type: "input",
+			name: "name",
+			alias: "n",
+			description: "Name for the test block",
+			message: "Enter a name for the test block.",
 			validate: validateName,
 		});
 
 		this.optionPrompt({
-			type: 'input',
-			name: 'filename',
-			alias: 'f',
-			description: 'Name for the test file (no extension)',
-			message: 'Enter a name for the test file (no extension)',
+			type: "input",
+			name: "filename",
+			alias: "f",
+			description: "Name for the test file (no extension)",
+			message: "Enter a name for the test file (no extension)",
 			default: () => getDefaultFilename(this.options),
 			validate: validateFilename,
 		});
@@ -32,7 +32,7 @@ class TestGenerator extends Generator {
 	addTest() {
 		// Create the new test file in the integration test directory.
 		this.copyTemplate(
-			'test.ts',
+			"test.ts",
 			`test/integration/${this.options.filename}.ts`,
 		);
 	}
--- generators/app/utils.js
+++ generators/app/utils.js
@@ -1,15 +1,15 @@
-const decamelize = require('decamelize');
+const decamelize = require("decamelize");
 
 exports.validateName = function(name) {
 	return (/^[\w- ]+$/).test(name) ||
-		'Test name may contain only word characters, dashes, and spaces';
+		"Test name may contain only word characters, dashes, and spaces";
 };
 
 exports.validateFilename = function(filename) {
 	return (/^[a-z0-9-]+$/).test(filename) ||
-		'File name must be dash-separated lower case';
+		"File name must be dash-separated lower case";
 };
 
 exports.getDefaultFilename = function(name) {
-	return decamelize(name, '-').replace(/[\W_]+/g, '-');
+	return decamelize(name, "-").replace(/[\W_]+/g, "-");
 };
